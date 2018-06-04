bdc674ca02f561e4e361cc18d50888d758c66955
micochan <micochan@qq.com>
2018-06-04 13:35:25 +0800

    add chapter6_1.c

---
+++ study_c/chapter6_1.c
@@ -0,0 +1,18 @@
+//Enter 3 integers and rank them from small to large
+#include <stdio.h>
+int main(void)
+{
+	int line;
+	int num = 1;
+	int m;
+	while(num <= line){
+		m = 0
+		while(m < num){
+		printf("$");
+		m++;
+		}
+	printf("\n");
+	num++;
+	}
+return 0;
+}

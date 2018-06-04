4bb0439ffca92ad82f96b4b6ab9e11a8e1d2e17e
micochan <micochan@qq.com>
2018-06-04 23:36:57 +0800

    add practice4.c

---
+++ study_c/chapter6/practice4.c
@@ -0,0 +1,16 @@
+//chapter6_practice3
+#include <stdio.h>
+int main(void)
+{
+	char ch = 'A';
+	int i, j;
+
+	for(i = 0; i < 6; i++){
+		for(j = 0; j <= i; j++){	
+		printf("%c",ch++);
+		}
+	printf("\n");
+	}
+
+return 0;
+}

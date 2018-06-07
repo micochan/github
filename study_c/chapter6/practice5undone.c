33271dd5a439026e18d1bb8b93649f2ef668f515
micochan <micochan@qq.com>
2018-06-07 22:32:09 +0800

    modified practice5.c

--- study_c/chapter6/practice5undone.c
+++ study_c/chapter6/practice5undone.c
@@ -2,41 +2,26 @@
 #include <stdio.h>
 int main(void)
 {
-int line;
-int num = 0;
-int space;
-int up;
-int down;
-char ch;
-char temp;
+int i,j,line;
+char ch,temp;
 
-	printf("Please input a uppercase latter:");
-	scanf("%c",&ch);
+printf("Please enter a uppercase:");
+scanf("%c",&ch);
+line = ch - 'A' +1;
 
-	line = ch - 'A' +1;
-    while(num < line){
-
-        space = line;
-        while(space >= num + 1){
+    for( i = 0; i < line; ++i ){
+        for(j = i; j < line; ++j){
         printf(" ");
-        space--;
-        }
-
-		down = num + 'A';
-        while(down >= 'A'){
-        printf("%c",down);
-        down--;
         }
         temp = 'A';
-		up = 0;
-        while(up < num){
-        printf("%c",++temp);
-        up++;
+        for(j = 0; j < i + 1;++j){
+        printf("%c",temp++);
+        }
+        for(j = 0; j < i ; ++j){
+        printf("%c",--temp);
         }
 
-	printf("\n");
-    num++;
+    printf("\n");
     }
-
 return 0;
 }

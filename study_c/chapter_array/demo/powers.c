9939ff1ac8c06c8eb53cfbc4b19da4479f88bba7
micochan <micochan@qq.com>
2018-06-27 00:15:07 +0800

    power.c

--- study_c/chapter_array/demo/powers.c
+++ study_c/chapter_array/demo/powers.c
@@ -13,9 +13,13 @@ int main(void){
 
 	int count = 5;		//数组元素长度不能使用变量
 	double deletePower;	//用户所要删除的战力值
-	int deleteIndex;	//要删除的战力值对应的元素下标
+	int deleteIndex = -1;	//要删除的战力值对应的元素下标
 	int i;				//循环变量
 	double powers[] = {42322, 45771, 40907, 41234, 40767};
+	for(i = 0; i < count; i++){
+	printf("%.2lf\t",powers[i]);
+	}
+	printf("\n");
 	printf("请输入要删除的战力值：");
 	scanf("%lf",&deletePower);
 
@@ -25,7 +29,20 @@ int main(void){
 			break;		//查找到后，跳出循环
 		}
 	}
-	printf("%d",deleteIndex);
+	if(deleteIndex == -1){
+	printf("您要删除的战力值不存在！\n");
+	return 1;
+	}
+	else{
+		for(i = deleteIndex; i < count - 1; i++){
+			powers[i] = powers[i + 1];
+		}
+	count--;
+	}
+	for(i = 0; i < count; i++){
+	printf("%.2lf\t",powers[i]);
+	}
+	printf("\n");
 return 0;
 }
 

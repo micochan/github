5cdf7a490a4e9ff2ddf8153363ba608c1c3b45c6
micochan <micochan@qq.com>
2018-07-04 21:50:44 +0800

    修改array2.c

--- study_c/chapter_array/demo/array2.c
+++ study_c/chapter_array/demo/array2.c
@@ -1,11 +1,11 @@
 //The two-dimensional array.二维数组
 #include <stdio.h>
-#define MONTHS 12   //The month of a year.每年１２个月
-#define YEARS	5	//5 years.总共５年
+#define MONTHS 12   //The month of a year.每年12个月
+#define YEARS	5	//5 years.总共5年
 
 void main()
 {
-	//2010~2014 amount of precipitation initialize array.　用２０１０～２０１４年的降水量数据初始化数组
+	//2010~2014 amount of precipitation initialize array.　用2010~2014年的降水量数据初始化数组
 	const float rain[YEARS][MONTHS] = 
 		{
 			{4.3, 4.3, 4.3, 3.0, 2.0, 1.2, 0.2, 0.2, 0.4, 2.4, 3.5, 6.6},
@@ -14,7 +14,7 @@ void main()
 			{7.2, 9.9, 8.4, 3.3, 1.2, 0.8, 0.4, 0.0, 0.6, 1.7, 4.3, 6.2},
 			{7.6, 5.6, 3.8, 2.8, 3.8, 0.2, 0.0, 0.0, 0.0, 1.3, 2.6, 5.2}
 		};	
-	int year, month;//循环变量year 控制行，month控制列
+	int year, month;//循环变量year，month ,外层循环控制行，内层循环控制列
 	float subtotal, total;   //每一年的降水量合计，５年总降水量合计
 
 	printf(" YEAR	RAINFALL (inches)\n");
@@ -26,7 +26,7 @@ void main()
 			}
 		
 		printf("%5d %15.1f\n", 2010 + year, subtotal);
-		total += subtotal;  //５年的总降水量 
+		total += subtotal;  //5年的总降水量 
 	}
 	printf("\nThe yearly average is %.1f inches.\n\n", total / YEARS); //5年平均每年的降水量
 	printf("MONTHLY AVERAGE:\n\n");
